84d8a2f4b9981edd5576b7c388f3122172dbd900
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-11-19 16:56:14 -0600

    going through conditional rendering

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React, { Components } from 'react';
+import ReactDOM from 'react-dom';
+
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    } else {
+        return <GuestGreeting />;
+    }
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
+// Let's create a stateful component that will render Login or
+// Logout depending on the current state. Will also render
+// <Greeting /> from earlier
+class LoginControl extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {
+            isLoggedIn: false
+        };
+    }
+
+    handleLoginClick() {
+        this.setState({
+            isLoggedIn: true
+        });
+    }
+
+    handleLogoutClick() {
+        this.setState({
+            isLoggedIn: false
+        });
+    }
+}
+
+ReactDOM.render(
+    // // See what happens if we set isLoggedIn={true}
+    // <Greeting isLoggedIn={false} />,
+    document.getElementById('root')
+);
