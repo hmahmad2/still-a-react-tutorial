83ff002a6142b3b2ad5a303c1d2bcc898b9bca05
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-12-04 13:53:43 -0600

    uniqueness of keys

--- src/index.js
+++ src/index.js
@@ -14,34 +14,82 @@ console.log(doubled);
 // );
 
 // this is a last resort sort of thing
-const todoItems = todo.map((todo, index) => 
-    // Only do this if you have no stable IDs
-    <li key={index}>
-        {todo.text}
-    </li>
-);
+// const todoItems = todo.map((todo, index) => 
+//     // Only do this if you have no stable IDs
+//     <li key={index}>
+//         {todo.text}
+//     </li>
+// );
 
 // const listItems = numbers.map((number) => 
 //     <li>{number}</li>
 // );
+
+// Keys only make sense in the surrounding array.
+function ListItem(props) {
+    // No need to specify a key here in this scenario
+    return <li>{props.value}</li>;
+}
+
 // Basic list Component
 function NumberList(props) {
     const numbers = props.numbers;
+
+    // Good rule of thumb: elements inside the map() call need keys
     const listItems = numbers.map((number) =>
         // keys help React identify which items hace changed,
         // are added, or are removed. Should be given to the elements
         // inside the array to give the elements a stable identity.
-        <li key={number.toString()}>
-            {number}
-        </li>
+        // {/* <li key={number.toString()}>
+        //     {number}
+        // </li> */}
+        
+        // In this scenario, the key should be specified inside array
+        <ListItem key={number.toString()} value={number} />
     );
     return (
-        <ul>{listItems}</ul>
+        <ul>
+            {listItems}
+        </ul>
     );
 }
 
+// Keys within arrays should be unique among their siblings. Don't
+// need to be globally unique. Can produce same keys when we produce two
+// different arrays
+function Blog(props) {
+    const sidebar = (
+        <ul>
+            {props.posts.map((post) =>
+                <li key={post.id}>
+                    {post.title}
+                </li>
+            )}
+        </ul>
+    );
+    const content = props.posts.map((post) =>
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
+    );
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    );
+}
+
+const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm'}
+];
+
 ReactDOM.render(
     // {/* <ul>{listItems}</ul>, // displays a bulletlist of numbers between 1 and 5 */}
-    <NumberList numbers={numbers} />,
+    // {/* <NumberList numbers={numbers} />, */}
+    <Blog posts={posts} />,
     document.getElementById('root')
 );
