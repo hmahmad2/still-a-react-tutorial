523aa2514b144f1d57b7e88394946a9a2dd887b1
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2020-01-19 13:00:51 -0600

    touched on controlling multiple inputs:

--- src/index.js
+++ src/index.js
@@ -110,9 +110,64 @@ class FlavorForm extends React.Component {
     }
 }
 
+class Reservation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            isGoing: true,
+            numberOfGuests: 2
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? 
+                target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+            // equivalent to:
+            /*
+            var partialState = {};
+            partialState[name] = value;
+            this.setState(partialState)
+            */
+           // in ES5
+        });
+    }
+
+    render() {
+        return (
+            <form>
+                <label>
+                    Is going:
+                    <input
+                        name="isGoing"
+                        type="checkbox"
+                        checked={this.state.isGoing}
+                        onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                    Number of guests:
+                    <input
+                        name="numberOfGuests"
+                        type="number"
+                        value={this.state.numberOfGuests}
+                        onChange={this.handleInputChange} />
+                </label>
+            </form>
+        );
+    }
+}
+
 ReactDOM.render(
     // {/* <NameForm />, */}
     // {/* <EssayForm />, */}
-    <FlavorForm />,
+    // {/* <FlavorForm />, */}
+    <Reservation />,
     document.getElementById('root')
 );
